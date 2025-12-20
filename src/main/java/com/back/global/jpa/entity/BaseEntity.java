827acce6ce9d9344bf1b8afb98e597e82376cb58
jhs512 <jangka512@gmail.com>
2025-12-20 23:39:10 +0900

    0015

--- src/main/java/com/back/global/jpa/entity/BaseEntity.java
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -1,15 +1,12 @@
 package com.back.global.jpa.entity;
 
 import com.back.global.global.GlobalConfig;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 @Getter
 public abstract class BaseEntity {
     public abstract int getId();
